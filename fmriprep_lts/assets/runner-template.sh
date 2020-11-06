1d449337cf4ae08dd9536b632e6b4bad93888377
jungheejung <heejung.jung@colorado.edu>
2020-11-06 16:14:43 -0500

    fmriprep for LTS - updated w/ parameters discussed with Tor & Martin

--- fmriprep_lts/assets/runner-template.sh
+++ fmriprep_lts/assets/runner-template.sh
@@ -1,17 +1,11 @@
 # Import Agave runtime extensions
 . _lib/extend-runtime.sh
 
-# Allow CONTAINER_IMAGE over-ride via local file
-if [ -z "${CONTAINER_IMAGE}" ]
-then
-    if [ -f "./_lib/CONTAINER_IMAGE" ]; then
-        CONTAINER_IMAGE=$(cat ./_lib/CONTAINER_IMAGE)
-    fi
-    if [ -z "${CONTAINER_IMAGE}" ]; then
-        echo "CONTAINER_IMAGE was not set via the app or CONTAINER_IMAGE file"
-        CONTAINER_IMAGE="jurrutia/ubuntu17"
-    fi
-fi
+# BUG Input Directory ${BIDS_DIRECTORY} not defined
+# using some bash tricks to get if from the participant label
+DIR=*/${PARTICIPANT_LABEL}
+DIR=$(echo ${DIR} | cut -d "/" -f1)
+echo Input is ${DIR}
 
 # Usage: container_exec IMAGE COMMAND OPTIONS
 #   Example: docker run centos:7 uname -a
@@ -21,30 +15,29 @@ mkdir -p  ${OUTPUT_DIR}/work
 PYTHONPATH=""
 # Echo command to std out
 echo container_exec ${CONTAINER_IMAGE} \
-               ${BIDS_DIRECTORY} \
+               ${DIR} \
                ${OUTPUT_DIR} \
                ${PARTICIPANT_LABEL} \
                -w  ${OUTPUT_DIR}/work \
                --write-graph \
                --n-cpus 16 \
                --notrack \
-               --fs-no-reconall \
-               --use-aroma  \
                --mem_mb 48000 \
+               ${FIELDMAPS} ${SLICE_TIMING} ${HEAD_MOTION} ${DUMMY_SCANS} \
+               ${ICA_AROMA_USE} ${ICA_AROMA_DIMENSIONALITY} ${FD_SPIKE} ${CIFTI_OUTPUT} \
                --fs-license-file /opt/license.txt
 
 container_exec ${CONTAINER_IMAGE} \
-               ${BIDS_DIRECTORY} \
+               ${DIR} \
                ${OUTPUT_DIR} \
                ${PARTICIPANT_LABEL} \
                -w  ${OUTPUT_DIR}/work \
                --write-graph \
                --n-cpus 16 \
                --notrack \
-               --fs-no-reconall \
-               --use-aroma  \
                --mem_mb 48000 \
+               ${FIELDMAPS} ${SLICE_TIMING} ${HEAD_MOTION} ${DUMMY_SCANS} \
+               ${ICA_AROMA_USE} ${ICA_AROMA_DIMENSIONALITY} ${FD_SPIKE} ${CIFTI_OUTPUT} \
                --fs-license-file /opt/license.txt
 
-
-rm -rf ${BIDS_DIRECTORY}
+rm -rf ${DIR}
