b50c3ce1266a5829a45f13b1a1c73e2d8e428fd6
jungheejung <>
2020-12-17 17:07:50 -0500

    update variable logic

--- heudiconv_app/assets/runner-template.sh
+++ heudiconv_app/assets/runner-template.sh
@@ -30,31 +30,170 @@ PYTHONPATH=""
 echo container_exec ${CONTAINER_IMAGE} \
                   heudiconv \
                  --dicom_dir_template ${DICOM_DIR_TEMPLATE} | --files ${FILES} \
-                 --subjects ${LIST_OF_SUBJECTS} \
-                 --converter {dcm2niix,none}] \
+                 ${LIST_OF_SUBJECTS} \
+                 ${CONVERTER} \
                  --outdir ${OUTDIR} \
-                 --locator ${LOCATOR} --conv-outdir ${CONV_OUTDIR} \
-                 --anon-cmd ${ANON_CMD} \
-                 --heuristic ${HEURISTIC} --with-prov \
-                 --ses ${SESSION_FOR_LONGITUDINAL} --bids --overwrite \
-                 --datalad] --dbg \
-                 --grouping {studyUID,accession_number} --minmeta \
-                 --random-seed ${RANDOM_SEED} --dcmconfig ${DCMCONFIG} \
+                 ${LOCATOR} ${CONV_OUTDIR} ${ANON_CMD} \
+                 --heuristic ${HEURISTIC} \
+                 ${SESSION_FOR_LONGITUDINAL} ${BIDS} ${OVERWRITE} \
+                 ${DATALAD} ${DCMCONFIG}
 
 
 container_exec ${CONTAINER_IMAGE} \
 heudiconv \
 --dicom_dir_template ${DICOM_DIR_TEMPLATE} | --files ${FILES} \
---subjects ${LIST_OF_SUBJECTS} \
---converter {dcm2niix,none}] \
+${LIST_OF_SUBJECTS} \
+${CONVERTER} \
 --outdir ${OUTDIR} \
---locator ${LOCATOR} --conv-outdir ${CONV_OUTDIR} \
---anon-cmd ${ANON_CMD} \
---heuristic ${HEURISTIC} --with-prov \
---ses ${SESSION_FOR_LONGITUDINAL} --bids --overwrite \
---datalad] --dbg \
---grouping {studyUID,accession_number} --minmeta \
---random-seed ${RANDOM_SEED} --dcmconfig ${DCMCONFIG} \
-
+${LOCATOR} ${CONV_OUTDIR} ${ANON_CMD} \
+--heuristic ${HEURISTIC} \
+${SESSION_FOR_LONGITUDINAL} ${BIDS} ${OVERWRITE} \
+${DATALAD} ${DCMCONFIG} 
 
 rm -rf ${DIR}
+
+
+
+
+"parameters": [
+
+{
+  "id": "LIST_OF_SUBJECTS",
+  "value": {
+    "default": "outputs",
+    "description": "list of subjects - required for dicom template. If not provided, DICOMS would first be “sorted” and subject IDs deduced by the heuristic",
+    "type": "string",
+    "visible": true,
+    "required": true
+  }
+},
+{
+  "id": "CONVERTER",
+  "details": {
+    "description": "",
+    "argument": "--converter ",
+    "showArgument": true,
+  },
+  "value": {
+    "default": "dcm2niix",
+    "type": "enumeration",
+    "enumValues": ["dcm2niix", "none"],
+    "visible": true,
+    "required": false
+  }
+},
+{
+  "id": "OUTDIR",
+  "value": {
+    "default": "outputs",
+    "description": "output directory for conversion setup (for further customization and future reference. This directory will refer to non-anonymized subject IDs",
+    "type": "string",
+    "visible": true,
+    "required": true
+  }
+},
+
+
+
+
+{
+  "id": "ANON_CMD",
+  "details": {
+    "argument": "--anon-cmd ",
+    "showArgument": true,
+    "description": "command to run to convert subject IDs used for DICOMs to anonymized IDs. Such command must take a single argument and return a single anonymized ID. Also see –conv-outdir"
+  },
+  "value": {
+    "default": 0,
+    "description": "command to run to convert subject IDs used for DICOMs to anonymized IDs. Such command must take a single argument and return a single anonymized ID. Also see –conv-outdir",
+    "type": "flag",
+    "visible": true,
+    "required": false
+  }
+},
+
+
+
+
+{
+  "id": "HEURISTIC",
+  "value": {
+    "default": "heuristic file (python)",
+    "description": "Name of a known heuristic or path to the Pythonscript containing heuristic",
+    "type": "string",
+    "visible": true,
+    "required": true
+  }
+},
+
+{
+  "id": "SESSION_FOR_LONGITUDINAL",
+  "details": {
+    "argument": "--ses ",
+    "showArgument": true,
+    "description": "session for longitudinal study_sessions, default is none"
+  },
+  "value": {
+    "default": 0,
+    "type": "flag",
+    "visible": true,
+    "required": false
+  }
+},
+
+{
+  "id": "BIDS",
+  "details": {
+    "argument": "--bids ",
+    "showArgument": true,
+    "description": "flag for output into BIDS structure"
+  },
+  "value": {
+    "default": 0,
+    "type": "flag",
+    "visible": true,
+    "required": false
+  }
+},
+
+{
+  "id": "OVERWRITE",
+  "details": {
+    "argument": "--overwrite ",
+    "showArgument": true,
+    "description": "flag to allow overwriting existing converted files"
+  },
+  "value": {
+    "default": 0,
+    "type": "flag",
+    "visible": true,
+    "required": false
+  }
+},
+{
+  "id": "DATALAD",
+  "details": {
+    "argument": "--datalad ",
+    "showArgument": true,
+    "description": "Store the entire collection as DataLad dataset(s). Small files will be committed directly to git, while large to annex. New version (6) of annex repositories will be used in a “thin” mode so it would look to mortals as just any other regular directory (i.e. no symlinks to under .git/annex). For now just for BIDS mode."
+  },
+  "value": {
+    "default": 0,
+    "type": "flag",
+    "visible": true,
+    "required": false
+  }
+},
+
+
+
+{
+  "id": "DCMCONFIG",
+  "value": {
+    "default": "outputs",
+    "description": "JSON file for additional dcm2niix configuration",
+    "type": "string",
+    "visible": true,
+    "required": true
+  }
+}]
