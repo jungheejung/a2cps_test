6bbfad80cf940addfbfb253c15d63f9b398df797
jungheejung <>
2021-02-15 16:36:15 -0500

    bids validator

---
+++ bidsvalidator_app/assets/runner-template.sh
@@ -0,0 +1,43 @@
+# Import Agave runtime extensions
+. _lib/extend-runtime.sh
+
+# Allow CONTAINER_IMAGE over-ride via local file
+if [ -z "${CONTAINER_IMAGE}" ]
+then
+    if [ -f "./_lib/CONTAINER_IMAGE" ]; then
+        CONTAINER_IMAGE=$(cat ./_lib/CONTAINER_IMAGE)
+    fi
+    if [ -z "${CONTAINER_IMAGE}" ]; then
+        echo "CONTAINER_IMAGE was not set via the app or CONTAINER_IMAGE file"
+        CONTAINER_IMAGE="jurrutia/ubuntu17"
+    fi
+fi
+
+
+# Usage: container_exec IMAGE COMMAND OPTIONS
+#   Example: docker run centos:7 uname -a
+#            container_exec centos:7 uname -a
+
+# Echo command to std out
+echo container_exec ${CONTAINER_IMAGE} \
+heudiconv \
+${DICOM_DIR_TEMPLATE} ${FILES} \
+${LIST_OF_SUBJECTS} \
+${CONVERTER} \
+--outdir ${OUTDIR} \
+${LOCATOR} ${CONV_OUTDIR} ${ANON_CMD} \
+${HEURISTIC} \
+${SESSION_FOR_LONGITUDINAL} ${BIDS} ${OVERWRITE} \
+${DATALAD} ${DCMCONFIG}
+
+
+container_exec ${CONTAINER_IMAGE} \
+heudiconv \
+${DICOM_DIR_TEMPLATE} ${FILES} \
+${LIST_OF_SUBJECTS} \
+${CONVERTER} \
+--outdir ${OUTDIR} \
+${LOCATOR} ${CONV_OUTDIR} ${ANON_CMD} \
+${HEURISTIC} \
+${SESSION_FOR_LONGITUDINAL} ${BIDS} ${OVERWRITE} \
+${DATALAD} ${DCMCONFIG}
